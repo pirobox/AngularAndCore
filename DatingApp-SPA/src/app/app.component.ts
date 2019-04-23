650d079c2e9e10dd4376a450a44cad25421cadee
Andrea Pirola <pirobox@hotmail.com>
2019-04-23 23:53:50 +0200

    initial commit

---
+++ DatingApp-SPA/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  aaaa() {
+    let x = 'aaaa';
+    if (true == true) {
+      const y = 'fdsfdskldsfk  dsf fjds jf jjf j f fjdsk';
+      let z = () => {
+        let zz = () => { };
+      }
+    }
+  }
+}
